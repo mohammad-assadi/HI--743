f1f98e0f95a2c3f1ab273980c6b90ac7c7d0a3f0
mohammad-assadi <76404542+mohammad-assadi@users.noreply.github.com>
2025-02-17 15:51:11 -0600

    Update Data Exploration.R

--- Data Exploration.R
+++ Data Exploration.R
@@ -8,33 +8,25 @@ health.data = NHANES
 view(NHANES)
 ?NHANES
 
-## dplyr usage
-### select 
 health.data = health.data %>% select(Age, Gender, BMI, BPsysAve,TotChol)
 
-### Filter
 health.data = health.data %>% filter(Age>50,)BMI>18.5
 
-### Mutate
 health.data = health.data%>% mutate(BMI_category=ifelse(BMI>25, "overweight","Normal""))
 
-### summary & group_by
 gender.summary = health.data%>% group_by(gender) %>% summarise(avg_BMI,na.rm=TRUE))
 
-#Missing values
 sum(is.na(health.data))
 colsums(is.na(health.data))
 
 health.data_no.mising = health.data %>% drop_na()
 
-#impute Missing values
-###Mean
+
 health.data = health.data %>% mutate(BPSysAve = ifelse(is.na(BPSysAve),mean(BPSysAve, na.rm = TRUE)))
 
-### Median 
 health.data = health.data %>% mutate(BPSysAve) = ifelse(is.na(BPSysAve),Median(BPSysAve,na.rm = TRUE))
 
-# cardinality
+
 sapply(health.data.function(x) lenght(unique(x)))
 
 
